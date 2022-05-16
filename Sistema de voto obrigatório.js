3e56aa9e1afca8ac1d7e3d92e0d612b8ead11834
Jaine Schlickmann Kestering <86730082+JaineSK@users.noreply.github.com>
2022-05-16 14:13:56 -0300

    Add files via upload

---
+++ Sistema de voto obrigatório.js
@@ -0,0 +1,14 @@
+var idade = 15
+if (idade < 18) {
+    console.log('Menor de idade')
+} else {
+    console.log('Maior de idade')
+}
+var idade = 15
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18) {
+    console.log('Voto opcional')
+} else if (idade >= 18) {
+    console.log('Voto obrigatório')
+}
